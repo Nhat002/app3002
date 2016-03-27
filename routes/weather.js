9576867848260d995994d10f3003bed857bc1f58
NHAT TRAN <xuannhat25@gmail.com>
2016-03-27 23:34:00 +0800

    add weather data read

--- routes/weather.js
+++ routes/weather.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var WeatherData = require('./models/WeatherData.js');
+var WeatherData = mongoose.model('WeatherData');
 router.get('/',function(req,res,next){
     if(err) {next(err);}
     WeatherData.find({},function(err, data){
